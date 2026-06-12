c839a1611676afb0ab8edd3960c9e3b549b501df
sasmit yadav <sasmit@myyahoo.com>
2026-06-12 13:54:03 +0530

    fixed js

--- demo-site/vite.config.js
+++ demo-site/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
+  root: ".",
+  publicDir: "public",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'login.html'),
-        signup: resolve(__dirname, 'signup.html'),
-        article: resolve(__dirname, 'article.html'),
+        index: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        signup: resolve(__dirname, "signup.html"),
+        article: resolve(__dirname, "article.html"),
+        memoryGame: resolve(__dirname, "memory-game.html"),
+        quiz: resolve(__dirname, "quiz.html"),
+        survey: resolve(__dirname, "survey.html"),
+        typingTest: resolve(__dirname, "typing-test.html"),
       },
     },
   },
